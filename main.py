abbbbe79ec4c240c1f1149f4afb66784a0bf3413
leandrovsk <contas.center@gmail.com>
2023-03-27 02:21:27 -0300

    feat: task 3 completed

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from management.product_handler import get_product_by_id, get_products_by_type, add_product
+from management.product_handler import get_product_by_id, get_products_by_type, add_product, menu_report
 from menu import products
 from management.tab_handler import calculate_tab
 
 if __name__ == "__main__":
+    print(get_product_by_id(28))
+
+    print(get_products_by_type('drink'))
+
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food"
+    }
+
+    print(add_product(products, **new_product))
+    print(add_product([], **new_product))
+
     table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
     table_2 = [
         {"_id": 10, "amount": 3},
@@ -12,3 +27,5 @@ if __name__ == "__main__":
 
     print(calculate_tab(table_1))
     print(calculate_tab(table_2))
+
+    print(menu_report())
--- management/product_handler.py
+++ management/product_handler.py
@@ -2,17 +2,25 @@ from menu import products
 
 
 def get_product_by_id(id: int):
+
+    if type(id) != int:
+        raise TypeError("product id must be an int")
+
     for product in products:
         if product["_id"] == id:
             return product
     return {}
 
 
-def get_products_by_type(type: str):
+def get_products_by_type(product_type: str):
+
+    if type(product_type) != str:
+        raise TypeError("product type must be a str")
+
     product = [
         product
         for product in products
-        if product["type"] == type
+        if product["type"] == product_type
     ]
     return product
 
@@ -33,3 +41,31 @@ def add_product(lista: list, **args):
     lista.append(args)
 
     return args
+
+def menu_report():
+
+    product_count = products.__len__()
+
+    product_sum = 0
+
+    for product in products:
+        product_sum += product["price"]
+
+    product_types = ["vegetable", "fruit", "drink", "dairy", "bakery", "meat"]
+
+    max_ocurr_type = ""
+    max_ocurr = 0
+    counter = 0
+
+    for item_type in product_types:
+        for product in products:
+            if product["type"] == item_type:
+                counter += 1
+        if counter > max_ocurr:
+            max_ocurr_type = item_type
+            max_ocurr = counter
+        counter = 0
+
+    average_price = round((product_sum / product_count), 2)
+
+    return f"Products Count: {product_count} - Average Price: ${average_price} - Most Common Type: {max_ocurr_type}"
